d439ffee7e6d38e12157bc1ac641d18cb0ce77e5
Shambhu Gohel <gohelshambhu95@yahoo.com>
2021-05-07 13:50:07 +0530

    Added Leftside Components

---
+++ src/components/main.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+import React from "react";
+
+const main = (pros) => {
+  return <Container>main</Container>;
+};
+
+const Container = styled.div`
+  grid-area: main;
+`;
+
+export default main;
---
+++ src/components/rightside.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+import React from "react";
+
+const rightside = (props) => {
+  return <Container>rightside</Container>;
+};
+
+const Container = styled.div`
+  grid-area: rightside;
+`;
+
+export default rightside;
